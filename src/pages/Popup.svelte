21978184c1cb71c3498f19d2a003e07daad2381c
swishyy <68352336+kakajuro@users.noreply.github.com>
2025-06-18 23:11:05 +0100

    Popup layout designed

--- src/pages/Popup.svelte
+++ src/pages/Popup.svelte
@@ -1,15 +1,17 @@
-<script lang="ts">
-  console.log("Hello from the popup!");
-</script>
 
-<div>
-  <img src="/icon-with-shadow.svg" alt="" />
-  <h1>vite-plugin-web-extension</h1>
-  <p>
-    Template: <code>svelte-ts</code>
-  </p>
-</div>
+<main class="w-[300px] h-[450px] p-6 flex flex-col">
+  <div class="w-screen">
+    Top Bar
+  </div>
+  <div class="flex flex-col items-center justify-center pt-12">
+    <img
+      src="/removeaioverviewlogo.png"
+      class="max-w-48 max-h-48"
+      alt="Remove AI Overview Logo"
+    />
+    <h1 class="pt-6 pb-4">Remove AI Overview</h1>
+    <h1>Slider here</h1>
+  </div>
 
-<style>
+</main>
 
-</style>
