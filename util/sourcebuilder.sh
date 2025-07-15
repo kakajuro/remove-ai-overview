39919b43ec149565a339ba1c9b4dd73ff594fc6a
swishyy <68352336+kakajuro@users.noreply.github.com>
2025-07-15 17:27:14 +0100

    Added source code zipping script

---
+++ util/sourcebuilder.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Remove previous data if any
+echo Setting up...
+rm -rf raosource raosource.zip
+mkdir raosource
+
+cd ..
+
+# In root directory, copy all needed files
+echo Copying files...
+cp -r manifests util/raosource
+cp -r public util/raosource
+cp -r src util/raosource
+cp util/sourceinfo.md util/raosource && cd util/raosource && mv sourceinfo.md README.md && cd ../..
+cp $(ls -pa | grep -v / | grep -v md | grep -v txt) util/raosource # Copies all files I want from root dir (all files except md and txt files)
+
+# Zip the source
+echo Zipping...
+cd util
+tar.exe acf raosource.zip -C raosource .
+rm -rf raosource
+echo Complete.
---
+++ util/sourceinfo.md
@@ -0,0 +1,37 @@
+## Building Locally
+
+Install dependencies (using yarn)
+
+```bash
+  yarn
+```
+
+Start the extension with to run in development mode
+
+```bash
+  yarn app:YOUR_PLATFORM-dev
+```
+
+Extension output locations
+
+```bash
+  ./dist/YOUR_PLATFORM_dist
+```
+
+Build the extension with
+
+```bash
+  yarn app:YOUR_PLATFORM-dev
+```
+
+or build and zip for all platforms at once using
+
+```bash
+  yarn build:all
+```
+
+These will be available at
+
+```bash
+  ./dist/rao_YOUR_PLATFORM_dist_v[VERSION].zip
+```
