f942b86f34046860bd6c9338dddc604e9522983a
swishyy <68352336+kakajuro@users.noreply.github.com>
2026-01-20 18:27:58 +0000

    improve update logic

--- src/rao-background.ts
+++ src/rao-background.ts
@@ -1,5 +1,16 @@
 import browser from "webextension-polyfill";
 
 browser.runtime.onInstalled.addListener((details) => {
-  console.log("remove-ai-overview installed!");
+  if(details.reason == "install"){
+    console.log("rao installed!");
+  } else if(details.reason == "update"){
+    console.log("extension updated");
+    browser.tabs.create({ url: "https://removeaioverview.com/updated" });
+  }
 });
+
+browser.runtime.onUpdateAvailable.addListener(details => {
+  console.log("Updating to: " + details.version);
+  browser.runtime.reload();
+});
+
